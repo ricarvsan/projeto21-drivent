37d6a7f3ea8322007e90eed9988135fb13273679
Ricardo Carvalho dos Santos <ricarvsan@gmail.com>
2023-11-13 08:40:45 -0300

    chore: fix deploy

--- src/routers/booking-router.ts
+++ src/routers/booking-router.ts
@@ -9,6 +9,6 @@ bookingRouter
   .all('/*', authenticateToken)
   .get('/', getBooking)
   .post('/', validateBody(bookingSchema), postBooking)
-  .put('/:bookingId', validateBody(bookingSchema), updateBooking)
+  .put('/:bookingId', validateBody(bookingSchema), updateBooking);
 
 export { bookingRouter };
